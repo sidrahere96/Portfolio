0b0cee2a49eaca0d3f03e1e169a70d236ce0bc44
sidrahere96 <sidrasc05@gmail.com>
2025-11-10 23:28:50 +0530

    ...

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,6 +2,13 @@ import React from "react"
 import Navbar from "./components/Navbar";
 import About from "./section/About";
 import Home from "./section/Home";
+import Skills from "./section/Skills";
+import Projects from "./section/Projects";
+import Contact from "./section/Contact";
+import Experience from "./section/Experience";
+import Services from "./section/Services";
+import Testimonials from "./section/Testimonials";
+import Footer from "./components/Footer";
 
 export default function App() {
   return(
