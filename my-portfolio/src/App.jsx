a3d9b01eaf347e01b430281e889a559d1e89222d
sidrahere96 <sidrasc05@gmail.com>
2025-11-10 20:13:39 +0530

    going with the flow

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,15 @@
+import Navbar from "./components/Navbar";
+import About from "./section/About";
+import Home from "./section/Home";
 
-
-function App() {
-
-  return (
-    <>
+export default function App() {
+  return(
+    <div>
+      <Navbar />
+      <Home />
+      <About />
       
-    </>
+    </div>
   )
-}
 
-export default App
+}
