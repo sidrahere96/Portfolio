a635edc6c483b98db14b79cf7e92bbe10f6ebead
sidrahere96 <sidrasc05@gmail.com>
2025-11-18 20:16:53 +0530

    the innovative cursor movement feature.

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar";
 import About from "./section/About";
+import CustomCursor from "./components/CustomCursor";
 import Home from "./section/Home";
 import Skills from "./section/Skills";
 import Project from "./section/Project";
@@ -14,7 +15,7 @@ export default function App() {
   return (
     <div className="relative gradient-bg text-white">
       <ParticlesBackground />
-
+      <CustomCursor />
       <Navbar />
       <Home />
       <About />
