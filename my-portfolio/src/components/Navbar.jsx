5a271bdbef263d3e647a16f4144674d59ede6b86
sidrahere96 <sidrasc05@gmail.com>
2025-11-20 14:41:40 +0530

    navbar positioning

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import OverlayMenu from ".OverlayMenu";
-export default function Navbar(){
-  const () => {
+import OverlayMenu from "./OverlayMenu";
+import { useState } from "react";
+import logo from "../assets/logo.png"
 
-  }
+export default function Navbar(){
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
+
 
 
   return (
     <>
-  <Nav>
+  <nav className={`fixed top-0 left-0 w-full flex items-centre justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" :"-translate-y-full"}`}>
+  <div>
+  <img src={logo} alt="Logo" className="h-8 w-8"/>
+  <div className="text-2xl font-bold text-white hidden sm:block">Sidra</div>
 
-  </Nav>
+  </div>
+  </nav>
     <OverlayMenu />
     </>
   );
