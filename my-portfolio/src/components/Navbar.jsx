ad791a8396576826d8f115a48e13796db458310a
sidrahere96 <sidrasc05@gmail.com>
2025-11-22 22:58:32 +0530

    navbar almost done

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import OverlayMenu from "./OverlayMenu";
-import { useState } from "react";
+import React,{ useState } from "react";
 import logo from "../assets/logo.png"
 
 export default function Navbar(){
@@ -11,9 +11,10 @@ export default function Navbar(){
   return (
     <>
   <nav className={`fixed top-0 left-0 w-full flex items-centre justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" :"-translate-y-full"}`}>
-  <div>
-  <img src={logo} alt="Logo" className="h-8 w-8"/>
-  <div className="text-2xl font-bold text-white hidden sm:block">Sidra</div>
+  <div className="flex item-centre space-x-2">
+  <img src={logo} alt="Logo" className="h-12 w-12"/>
+  <div className="text-2xl font-bold text-white hidden sm:block">
+    Sidra</div>
 
   </div>
   </nav>
