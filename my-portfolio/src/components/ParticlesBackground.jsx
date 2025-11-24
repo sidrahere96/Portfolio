2c99bbb286ec22070c9301d645c5334565df8647
sidrahere96 <sidrasc05@gmail.com>
2025-11-24 19:42:44 +0530

    ...

--- my-portfolio/src/components/ParticlesBackground.jsx
+++ my-portfolio/src/components/ParticlesBackground.jsx
@@ -14,7 +14,7 @@ export default function ParticlesBackground() {
     resizeCanvas();
 
     let particles = [];
-    const particleCount = 200;
+    const particleCount = 300;
 
     const mouse = { x: null, y: null };
 
