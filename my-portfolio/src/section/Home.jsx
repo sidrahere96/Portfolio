e2807604712218bf88b159a5139e9f3ccf97c82f
sidrahere96 <sidrasc05@gmail.com>
2025-12-31 20:44:33 +0530

    working on home section

--- my-portfolio/src/section/Home.jsx
+++ my-portfolio/src/section/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import ParticlesBackground from '../components/ParticlesBackground';
 
-const Home = () => {
+export default function Home() {
   return (
-    <div className='w-full h-screen'>
-      Home
-    </div>
-  )
+    <section id="home" className="w-full h-screen relative bg-black overflow-hidden flex items-center justify-center">
+    <ParticlesBackground />
+    </section>
+  );
 }
-
-export default Home
